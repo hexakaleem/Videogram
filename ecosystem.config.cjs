a459a548ec835394bba0f321a74f34c390fdc8a2
Kaleem Hassan <kaleemhassanansari@gmail.com>
2026-03-14 14:49:22 +0500

    push

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
                 PM2_SERVE_PATH: './frontend/dist',
                 PM2_SERVE_PORT: 8080,
                 PM2_SERVE_SPA: 'true',
-                PM2_SERVE_HOMEPAGE: './index.html'
+                PM2_SERVE_HOMEPAGE: '/index.html'
             }
         }
     ]
