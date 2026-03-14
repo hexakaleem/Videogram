bedb7d178d9feef0f5afd8b6d5c7e4f5a74fb7fe
Kaleem Hassan <kaleemhassanansari@gmail.com>
2026-03-14 15:39:23 +0500

    dotenv

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,45 +1,45 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs'
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config({ quiet: true })
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
 })
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if(!localFilePath){
-            console.error(`No Local File Path`)
-            return null
-        }
-
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: 'auto'
-        })
-
-        console.log(`File is Uploaded Successfully: ${response.url}`)
-        fs.unlink(localFilePath, (error) => {
-          if(error){
-            console.log(error)
-          }
-        })
-        return response
-
-    } catch (error) {
-        console.log(`Error Uploading File on Cloudinary: ${error.message}`)
-        fs.unlink(localFilePath, (error) => {
-          if(error){
-            console.log(error)
-          }
-        })
-        return null
+  try {
+    if (!localFilePath) {
+      console.error(`No Local File Path`)
+      return null
     }
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto'
+    })
+
+    console.log(`File is Uploaded Successfully: ${response.url}`)
+    fs.unlink(localFilePath, (error) => {
+      if (error) {
+        console.log(error)
+      }
+    })
+    return response
+
+  } catch (error) {
+    console.log(`Error Uploading File on Cloudinary: ${error.message}`)
+    fs.unlink(localFilePath, (error) => {
+      if (error) {
+        console.log(error)
+      }
+    })
+    return null
+  }
 }
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary }
 
 
 /*
